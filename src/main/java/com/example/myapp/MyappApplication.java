67684d75db481e62b8b6d7f25de4599e2be9f715
tudulius <tudulius@gmail.com>
2024-11-23 07:47:23 +0000

    ok

---
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class MyappApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyappApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "Spring Boot App";
+	}
+}
